3b9a2fedede51294146c7dde15bf62ac22693c68
Abdul <hafizhahm123@gmail.com>
2024-10-09 09:23:34 +0700

    styling: hero & title

--- src/scripts/components/hero-app.js
+++ src/scripts/components/hero-app.js
@@ -9,6 +9,12 @@ class HeroApp extends HTMLElement {
       this.innerHTML = `
         <div class="hero-img">
         <img src="./images/heros/hero-image_2.jpg" alt="hero-image-1">
+        <div class="hero-text">
+        Enjoy deliciousness that <br>
+        can captivate your taste buds!. <br>
+
+        
+        </div>
        </div>
           
           
