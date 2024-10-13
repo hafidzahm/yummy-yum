b23ca842ec99385d660bfc04d9ea5b5be6251dc2
Abdul <hafizhahm123@gmail.com>
2024-10-13 18:06:35 +0700

    responsive-saved

--- src/scripts/components/hero-app.js
+++ src/scripts/components/hero-app.js
@@ -8,10 +8,10 @@ class HeroApp extends HTMLElement {
     render() {
       this.innerHTML = `
         <div class="hero-img">
-        <img src="./images/heros/hero-image_2.jpg" alt="hero-image-1">
-        <div class="hero-text">
+        <img id="hero-img"src="./images/heros/hero-image_2.jpg" alt="hero-image-1">
+        <div class="hero-text" id="hero-text">
         Enjoy deliciousness that <br>
-        can captivate your taste buds!. <br>
+        can captivate your taste buds! <br>
 
         
         </div>
