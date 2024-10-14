51f4961c5896a2b4f3767634a3b5b522d61d6152
Abdul <hafizhahm123@gmail.com>
2024-10-14 16:21:23 +0700

    add: navbar

--- src/scripts/components/header-app.js
+++ src/scripts/components/header-app.js
@@ -8,15 +8,16 @@ class HeaderApp extends HTMLElement {
   render() {
     this.innerHTML = `
 
-    <div class="topnav">
-    <div class="brand-name">
-    <img id="brand-logo" src="./images/heros/brand.png" alt="yummy-logo">
-    </div>
-    <a href="https://github.com/hafidzahm">ABOUT US</a>
-    <a href="#">FAVOURITE</a>
-    <a href="#home">HOME</a>
-
-  </div> 
+    <div class="nav-container">
+    <img id="nav-img" src="./images/heros/brand.png" alt="hero-image-1" />
+    <nav>
+      <ul>
+          <li><a href="#root">HOME</a></li>
+          <li><a href="#">FAVOURITE</a></li>
+          <li><a href="https://github.com/hafidzahm">ABOUT US</a></li>
+      </ul>
+    </nav>
+  </div>
 
         
         
