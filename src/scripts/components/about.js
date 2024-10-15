19827671ba19b9fc07dec8b4db981ffa4dd8c30b
Abdul <hafizhahm123@gmail.com>
2024-10-15 16:26:36 +0700

    add: responsive about

--- src/scripts/components/about.js
+++ src/scripts/components/about.js
@@ -11,7 +11,7 @@ class AboutUs extends HTMLElement {
     <h1 id="about-title">About Us</h1>
     <div id="about-desc">
         <img id="about-img" src="./images/heros/hero-image_1.jpg" alt="about-image">
-        <div>
+        <div id="about-prg">
             <p id='about-paragraph'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt dolores fugiat hic eum, 
                 aliquam porro similique consequuntur! 
                 Consectetur velit doloribus optio dignissimos repellat suscipit itaque autem, 
